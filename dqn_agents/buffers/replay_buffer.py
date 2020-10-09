04cce9bb4650e5d73123c8d6de2f27494516df7e
volodymyr.kovenko <volodymyr.kovenko@vimmi.net>
2020-10-09 13:38:35 +0300

    fixed prioritized replay buffer, added new notebook

--- dqn_agents/buffers/replay_buffer.py
+++ dqn_agents/buffers/replay_buffer.py
@@ -75,40 +75,39 @@ class ReplayBuffer(object):
 class PrioritizedReplayBuffer(object):
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, buffer_size, batch_size=64, seed=5,
+    def __init__(self, buffer_size, seed=5,
                  compute_weights=True, alpha=0.5, beta=0.5):
         """Initialize a ReplayBuffer object.
-        Params
-        ======
+        Parameters
+        ----------
             buffer_size (int): maximum size of buffer
-            experiences_per_sampling (int): number of experiences to sample during a sampling iteration
             batch_size (int): size of each training batch
             seed (int): random seed
+            compute_weights (bool): if to compute weights for loss function
+            alpha (float): parameter for probabilities
+            beta (float): parameter for weights
         """
         self.buffer_size = buffer_size
-        self.batch_size = batch_size
         self.alpha = alpha
         self.alpha_decay_rate = 0.99
+        self.epsilon = 1e-11
         self.beta = beta
         self.beta_growth_rate = 1.001
         random.seed(seed)
         self.compute_weights = compute_weights
         self.experience_count = 0
-
-        self.experience = np.zeros(shape=(self.buffer_size, 5), dtype='object')
-        self.data = np.zeros(shape=(self.buffer_size, 4), dtype='object')
-
-        self.sampled_batches = []
-        self.current_batch = 0
+        self.current_size = 0
+        self.experience = [[0, 0, 0, 0, 0] for i in range(self.buffer_size)]
+        self.data = [[0, 0, 0, 0] for i in range(self.buffer_size)]
         self.priorities_sum_alpha = 0
-        self.priorities_max = 1
+        self.priorities_max = 1 + self.epsilon
         self.weights_max = 1
 
     def update_priorities(self, tds, indices):
         for td, index in zip(tds, indices):
             N = min(self.experience_count, self.buffer_size)
 
-            updated_priority = td
+            updated_priority = td + self.epsilon
             if updated_priority > self.priorities_max:
                 self.priorities_max = updated_priority
 
@@ -119,16 +118,16 @@ class PrioritizedReplayBuffer(object):
             else:
                 updated_weight = 1
 
-            old_priority = self.data[index, 0]
+            old_priority = self.data[index][0]
             self.priorities_sum_alpha += updated_priority ** self.alpha - old_priority ** self.alpha
             updated_probability = updated_priority ** self.alpha / self.priorities_sum_alpha
             self.data[index] = [updated_priority, updated_probability, updated_weight, index]
 
-    def _encode_samples(self):
+    def _encode_samples(self, batch_size):
         """Randomly sample a batch of experiences from memory."""
         return np.array(random.choices(self.data,
-                                       self.data[:, 1],
-                                       k=self.batch_size))
+                                       [i[1] for i in self.data],
+                                       k=batch_size))
 
     def update_parameters(self):
         self.alpha *= self.alpha_decay_rate
@@ -136,52 +135,61 @@ class PrioritizedReplayBuffer(object):
         if self.beta > 1:
             self.beta = 1
         N = min(self.experience_count, self.buffer_size)
-        self.priorities_sum_alpha = np.sum(self.data[:, 0] ** self.alpha)
+        self.priorities_sum_alpha = 0
         for element in self.data:
-            probability = element[0] ** self.alpha / self.priorities_sum_alpha
+            self.priorities_sum_alpha += element[0] ** self.alpha
+        for element in self.data:
+            probability = (element[0] ** self.alpha) / self.priorities_sum_alpha
             weight = 1
             if self.compute_weights:
                 weight = ((N * element[1]) ** (-self.beta)) / self.weights_max
-            self.data[int(element[-1]), :] = [element[0], probability, weight, element[-1]]
+            self.data[int(element[-1])] = [element[0], probability, weight, element[-1]]
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
         self.experience_count += 1
         index = self.experience_count % self.buffer_size
-
+        self.current_size += 1
         if self.experience_count > self.buffer_size:
             temp = self.data[index]
             self.priorities_sum_alpha -= temp[0] ** self.alpha
             if temp[0] == self.priorities_max:
-                self.data[index, 0] = 0
+                temp[0] = 0
+                self.data[index] = temp
                 self.priorities_max = max(self.data, key=lambda x: x[0])[0]
             if self.compute_weights:
                 if temp[2] == self.weights_max:
-                    self.data[index, 2] = 0
+                    temp[2] = 0
+                    self.data[index] = temp
                     self.weights_max = max(self.data, key=lambda x: x[2])[2]
-            self.experience_count = self.buffer_size
+            self.current_size = self.buffer_size
 
-        priority = self.priorities_max
+        priority = self.priorities_max + self.epsilon
         weight = self.weights_max
         self.priorities_sum_alpha += priority ** self.alpha
-        probability = priority ** self.alpha / self.priorities_sum_alpha
-        self.experience[index] = np.array([state, action, reward, next_state, done], dtype='object')
-        self.data[index] = np.array([priority, probability, weight, index], dtype='object')
+        probability = (priority ** self.alpha) / self.priorities_sum_alpha
+        self.experience[index] = [state, action, reward, next_state, done]
+        self.data[index] = [priority, probability, weight, index]
 
-    def sample(self):
-        sampled_batch = self._encode_samples()
+    def sample(self, batch_size=64):
+        sampled_batch = self._encode_samples(batch_size)
 
         weights = np.copy(sampled_batch[:, 2]).astype(float)
         idx = np.copy(sampled_batch[:, -1]).astype(int)
+        obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
 
-        states = np.stack(self.experience[idx, 0])
-        actions = np.stack(self.experience[idx, 1])
-        rewards = np.stack(self.experience[idx, 2])
-        next_states = np.stack(self.experience[idx, 3])
-        dones = np.stack(self.experience[idx, 4])
+        for i in idx:
+            elem = self.experience[i]
+            obs_t, action, reward, obs_tp1, done = elem
+            obses_t.append(np.array(obs_t, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(reward)
+            obses_tp1.append(np.array(obs_tp1, copy=False))
+            dones.append(done)
 
-        return states, actions, rewards, next_states, dones, np.array(weights), idx
+        return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), \
+               np.array(dones), np.array(weights), idx
 
     def __len__(self):
         """Return the current size of internal memory."""
-        return self.experience_count
+        return self.current_size
